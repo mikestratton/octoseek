94618248dc22c746ff839ec569f0da89d6eaeb81
mikestratton <mike@mikestratton.net>
2019-06-21 03:58:41 -0400

    initial commit

---
+++ resources/views/octo-data.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container text-center">
+        <div class="row mt-4">
+            <div class="col-md-12">
+                <img class="img-fluid" style="max-height: 250px;" src="images/octodata-logo.svg" alt="octodata logo">
+            </div>
+
+        </div>
+
+
+        <div class="row mt-4">
+            <div class="col-md-12">
+                <h2 class="text-justify">Octo Data is a Magical Creature & Seeker of Knowledge. It's primary purpose is in the use
+                    of machine learning to comb through the sea of big data, to help your business in locating useful
+                    perls of information.</h2>
+            </div>
+        </div>
+        <hr>
+        <div class="row mt-5">
+            <div class="col-md-12">
+                <h3>Supported Industries</h3>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-4">
+                <h5>Real Estate Development <br>
+                    NAICS Code 531390 <br>
+                </h5>
+            </div>
+            <div class="col-md-4">
+                <h5>Vending Machine Operators <br>
+                    NAICS Code 454210 <br>
+                </h5>
+            </div>
+            <div class="col-md-4">
+                <h5>Offices of Lawyers<br>
+                    NAICS Code 541110 <br>
+
+                </h5>
+            </div>
+        </div>
+
+
+
+                <div class="bottom-0-octo">
+
+
+                </div>
+
+
+    </div>
+@endsection
