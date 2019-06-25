eaf7144975aae4ca6cedc9fbf225dcc2e6c49551
mikestratton <mike@mikestratton.net>
2019-06-25 04:22:20 -0400

    fixed manufactures table migration error

--- database/migrations/2019_06_25_044144_create_manufactures_table.php
+++ database/migrations/2019_06_25_044144_create_manufactures_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Manufactures extends Migration
+class CreateManufacturesTable extends Migration
 {
     /**
      * Run the migrations.
