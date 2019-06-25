9d0e32bb9850eb4475a87aae6bacd4008bc91803
mikestratton <mike@mikestratton.net>
2019-06-25 00:42:17 -0400

    manufractiring table

---
+++ database/factories/OctopusFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Octopus;
+use Faker\Generator as Faker;
+
+$factory->define(Octopus::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/VendorFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Vendor;
+use Faker\Generator as Faker;
+
+$factory->define(Vendor::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
