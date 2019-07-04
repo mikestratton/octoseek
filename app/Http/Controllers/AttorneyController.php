4ebaf9c13316c3b36aed751f2aa350ccd95f0f90
mikestratton <mike@mikestratton.net>
2019-07-04 08:49:04 -0400

    attorneys demo complete

--- app/Http/Controllers/AttorneyController.php
+++ app/Http/Controllers/AttorneyController.php
@@ -10,9 +10,45 @@ class AttorneyController extends Controller
 {
     public function example(){
 
-        $attorneys = Attorney::all()->random(10);
+//        $attorneys = Attorney::where('id', 1)->get();
 
-        return view('data.attorneys.attorney-example', compact('attorneys'));
+        $att_web = Attorney::where('website', '!=', '')
+            ->inRandomOrder()
+            ->limit(20)
+            ->get();
+
+        $att_pho = Attorney::where('phone', '!=', '')
+            ->where('website', '!=', '')
+            ->inRandomOrder()
+            ->limit(20)
+            ->get();
+
+        $att_sal = Attorney::where('sales', '>', 1000000)
+            ->inRandomOrder()
+            ->limit(20)
+            ->get();
+
+        $att_add = Attorney::where('state', 'NY')
+            ->inRandomOrder()
+            ->limit(20)
+            ->get();
+
+        $att_alp = Attorney::where('state', 'NY')
+            ->where('credit_score_alpha', '!=', '')
+            ->inRandomOrder()
+            ->limit(20)
+            ->get();
+
+        $att_emp = Attorney::where('employees', '>', '50')
+            ->inRandomOrder()
+            ->limit(20)
+            ->get();
+
+//        $pag = Attorney::where('website', '!=', '')->paginate(15);
+
+
+        return view('data.attorneys.attorney-example',
+            compact('att_web', 'att_pho', 'att_sal', 'att_add', 'att_alp', 'att_emp'));
     }
 
     public function fiveThousand(){
--- resources/views/data/attorneys/attorney-example.blade.php
+++ resources/views/data/attorneys/attorney-example.blade.php
@@ -4,28 +4,196 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div class="card text-center">
+            <div class="card">
                 <div class="card-header text-light bg-primary">
-                    <h1>
+
                         Attorney Sample Data
-                    </h1>
+
 
                 </div>
 
                 <div class="card-body">
                     <h3 class="pt-3 pb-3 text-primary">
-                        Clicking on any button will return a list of 10 random records
+                       Our focus is on finding exactly what you need. If the data exists, we will find it.<br><br>
                     </h3>
 
                     <div id="tabs" class="ui-tabs ui-corner-all ui-widget ui-widget-content">
-                        <ul role="tablist" class="ui-tabs-nav ui-corner-all ui-helper-reset ui-helper-clearfix ui-widget-header">
-                            <li role="tab" tabindex="0" class="ui-tabs-tab ui-corner-top ui-state-default ui-tab ui-tabs-active ui-state-active" aria-controls="tabs-1" aria-labelledby="ui-id-15" aria-selected="true" aria-expanded="true"><a href="#tabs-1" role="presentation" tabindex="-1" class="ui-tabs-anchor" id="ui-id-15">First</a></li>
-                            <li role="tab" tabindex="-1" class="ui-tabs-tab ui-corner-top ui-state-default ui-tab" aria-controls="tabs-2" aria-labelledby="ui-id-16" aria-selected="false" aria-expanded="false"><a href="#tabs-2" role="presentation" tabindex="-1" class="ui-tabs-anchor" id="ui-id-16">Second</a></li>
-                            <li role="tab" tabindex="-1" class="ui-tabs-tab ui-corner-top ui-state-default ui-tab" aria-controls="tabs-3" aria-labelledby="ui-id-17" aria-selected="false" aria-expanded="false"><a href="#tabs-3" role="presentation" tabindex="-1" class="ui-tabs-anchor" id="ui-id-17">Third</a></li>
+                        <ul role="tablist" class="ui-tabs-nav ui-corner-all ui-helper-reset ui-helper-clearfix ui-widget-header ">
+                            <li role="tab" tabindex="0" class="ui-tabs-tab ui-corner-top ui-state-default ui-tab ui-tabs-active ui-state-active" aria-controls="tabs-1" aria-labelledby="ui-id-15" aria-selected="true" aria-expanded="true"><a href="#tabs-1" role="presentation" tabindex="-1" class="ui-tabs-anchor" id="ui-id-15">Website</a></li>
+                            <li role="tab" tabindex="-1" class="ui-tabs-tab ui-corner-top ui-state-default ui-tab" aria-controls="tabs-2" aria-labelledby="ui-id-16" aria-selected="false" aria-expanded="false"><a href="#tabs-2" role="presentation" tabindex="-1" class="ui-tabs-anchor" id="ui-id-16">Phone</a></li>
+                            <li role="tab" tabindex="-1" class="ui-tabs-tab ui-corner-top ui-state-default ui-tab" aria-controls="tabs-3" aria-labelledby="ui-id-17" aria-selected="false" aria-expanded="false"><a href="#tabs-3" role="presentation" tabindex="-1" class="ui-tabs-anchor" id="ui-id-17">Salary</a></li>
+                            <li role="tab" tabindex="-1" class="ui-tabs-tab ui-corner-top ui-state-default ui-tab" aria-controls="tabs-4" aria-labelledby="ui-id-18" aria-selected="false" aria-expanded="false"><a href="#tabs-4" role="presentation" tabindex="-1" class="ui-tabs-anchor" id="ui-id-18">Address</a></li>
+                            <li role="tab" tabindex="-1" class="ui-tabs-tab ui-corner-top ui-state-default ui-tab" aria-controls="tabs-5" aria-labelledby="ui-id-19" aria-selected="false" aria-expanded="false"><a href="#tabs-5" role="presentation" tabindex="-1" class="ui-tabs-anchor" id="ui-id-19">Credit</a></li>
+                            <li role="tab" tabindex="-1" class="ui-tabs-tab ui-corner-top ui-state-default ui-tab" aria-controls="tabs-6" aria-labelledby="ui-id-20" aria-selected="false" aria-expanded="false"><a href="#tabs-6" role="presentation" tabindex="-1" class="ui-tabs-anchor" id="ui-id-20">Employees</a></li>
                         </ul>
-                        <div id="tabs-1" aria-labelledby="ui-id-15" role="tabpanel" class="ui-tabs-panel ui-corner-bottom ui-widget-content" aria-hidden="false" style="display: block;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
-                        <div id="tabs-2" aria-labelledby="ui-id-16" role="tabpanel" class="ui-tabs-panel ui-corner-bottom ui-widget-content" aria-hidden="true" style="display: none;">Phasellus mattis tincidunt nibh. Cras orci urna, blandit id, pretium vel, aliquet ornare, felis. Maecenas scelerisque sem non nisl. Fusce sed lorem in enim dictum bibendum.</div>
-                        <div id="tabs-3" aria-labelledby="ui-id-17" role="tabpanel" class="ui-tabs-panel ui-corner-bottom ui-widget-content" aria-hidden="true" style="display: none;">Nam dui erat, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue.</div>
+                        <div id="tabs-1" aria-labelledby="ui-id-15" role="tabpanel" class="ui-tabs-panel ui-corner-bottom ui-widget-content" aria-hidden="false" style="display: block;">
+                           <h2 class="text-left pt-3 pb-3">Find Attorneys Websites</h2>
+                            @if($att_web)
+                                <table class="table table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th class="text-left">Attorney</th>
+                                            <th class="text-left">Website</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        @foreach($att_web as $attorney)
+                                            <tr>
+                                                <td class="text-left">{{ $attorney->company }}</td>
+                                                <td class="text-left">{{ $attorney->website }}</td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                           @else
+                            <p>Please check back later.</p>
+
+                            @endif
+
+                        </div>
+                        <div id="tabs-2" aria-labelledby="ui-id-16" role="tabpanel" class="ui-tabs-panel ui-corner-bottom ui-widget-content" aria-hidden="true" style="display: none;">
+                            @if($att_pho)
+                                <h2 class="text-left pb-3 pt-3">Find Attorneys Websites & Phone Numbers</h2>
+                                <table class="table table-hover">
+                                    <thead>
+                                    <tr>
+                                        <th class="text-left">Attorney</th>
+                                        <th class="text-left">Website</th>
+                                        <th class="text-left">Phone</th>
+
+                                    </tr>
+                                    </thead>
+
+                                    <tbody>
+                                    @foreach($att_pho as $attorney)
+                                        <tr>
+                                            <td class="text-left">{{ $attorney->company }}</td>
+                                            <td class="text-left">{{ $attorney->website }}</td>
+                                            <td class="text-left">{{ $attorney->phone }}</td>
+
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            @else
+                                <p>Please check back later.</p>
+
+                            @endif
+                        </div>
+                        <div id="tabs-3" aria-labelledby="ui-id-17" role="tabpanel" class="ui-tabs-panel ui-corner-bottom ui-widget-content" aria-hidden="true" style="display: none;">
+                            @if($att_pho)
+                                <h2 class="text-left pb-3 pt-3">Find Attorneys with an Estimated Yearly Salary Over $1,000,000</h2>
+                                <table class="table table-hover">
+                                    <thead>
+                                    <tr>
+                                        <th class="text-left">Attorney</th>
+                                        <th class="text-left">Salary</th>
+
+                                    </tr>
+                                    </thead>
+
+                                    <tbody>
+                                    @foreach($att_sal as $attorney)
+                                        <tr>
+                                            <td class="text-left">{{ $attorney->company }}</td>
+                                            <td class="text-left">{{ $attorney->sales }}</td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            @else
+                                <p>Please check back later.</p>
+
+                            @endif
+                        </div>
+
+                        <div id="tabs-4" aria-labelledby="ui-id-18" role="tabpanel" class="ui-tabs-panel ui-corner-bottom ui-widget-content" aria-hidden="true" style="display: none;">
+                            @if($att_add)
+                                <h2 class="text-left pb-3 pt-3">Find Addresses of Attorneys in the State of New York</h2>
+                                <table class="table table-hover">
+                                    <thead>
+                                    <tr>
+                                        <th class="text-left">Attorney</th>
+                                        <th class="text-left">Address</th>
+
+                                    </tr>
+                                    </thead>
+
+                                    <tbody>
+                                    @foreach($att_add as $attorney)
+                                        <tr>
+                                            <td class="text-left">{{ $attorney->company }}</td>
+                                            <td class="text-left">
+                                                {{ $attorney->address }}, {{ $attorney->city }}, {{ $attorney->state }} {{ $attorney->zip }}
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            @else
+                                <p>Please check back later.</p>
+
+                            @endif
+                        </div>
+
+                        <div id="tabs-5" aria-labelledby="ui-id-19" role="tabpanel" class="ui-tabs-panel ui-corner-bottom ui-widget-content" aria-hidden="true" style="display: none;">
+                            @if($att_alp)
+                                <h2 class="text-left pb-3 pt-3">Find the Alpha Credit Rating of Attorneys in the State of New York</h2>
+                                <table class="table table-hover">
+                                    <thead>
+                                    <tr>
+                                        <th class="text-left">Attorney</th>
+                                        <th class="text-left">City & State</th>
+                                        <th class="text-left">Rating</th>
+
+                                    </tr>
+                                    </thead>
+
+                                    <tbody>
+                                    @foreach($att_alp as $attorney)
+                                        <tr>
+                                            <td class="text-left">{{ $attorney->company }}</td>
+                                            <td class="text-left">{{ $attorney->city }}, {{ $attorney->state }}</td>
+                                            <td class="text-left">{{ $attorney->credit_score_alpha }} </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            @else
+                                <p>Please check back later.</p>
+
+                            @endif
+                        </div>
+
+
+                        <div id="tabs-6" aria-labelledby="ui-id-20" role="tabpanel" class="ui-tabs-panel ui-corner-bottom ui-widget-content" aria-hidden="true" style="display: none;">
+                            @if($att_emp)
+                                <h2 class="text-left pb-3 pt-3">Find Attorneys With More Then 50 Employees</h2>
+                                <table class="table table-hover">
+                                    <thead>
+                                    <tr>
+                                        <th class="text-left">Attorney</th>
+                                        <th class="text-left">Employees</th>
+
+                                    </tr>
+                                    </thead>
+
+                                    <tbody>
+                                    @foreach($att_emp as $attorney)
+                                        <tr>
+                                            <td class="text-left">{{ $attorney->company }}</td>
+                                            <td class="text-left">{{ $attorney->employees }} </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            @else
+                                <p>Please check back later.</p>
+
+                            @endif
+                        </div>
+
                     </div>
 
 
