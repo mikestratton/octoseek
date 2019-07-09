00467b916a1b9efa5e3f3c3faf85e35ad9db55c2
mikestratton <mike@mikestratton.net>
2019-07-08 23:46:51 -0400

    line endings, vendors:csv,migration

---
+++ database/migrations/2019_07_09_032005_add_columns_to_vendors_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToVendorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vendors', function (Blueprint $table) {
+            $table->string('company')->after('id');
+            $table->string('city')->after('id');
+            $table->string('state')->after('id');
+            $table->integer('zip')->after('id');
+            $table->string('address')->after('id');
+            $table->string('county')->after('id');
+            $table->string('phone')->after('id');
+            $table->string('fax')->after('id');
+            $table->string('name')->after('id');
+            $table->string('title')->after('id');
+            $table->string('gender')->after('id');
+            $table->integer('employees')->after('id');
+            $table->string('sales')->after('id');
+            $table->string('industry')->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vendors', function (Blueprint $table) {
+            $table->dropColumn('company');
+            $table->dropColumn('city');
+            $table->dropColumn('state');
+            $table->dropColumn('zip');
+            $table->dropColumn('address');
+            $table->dropColumn('county');
+            $table->dropColumn('phone');
+            $table->dropColumn('fax');
+            $table->dropColumn('name');
+            $table->dropColumn('title');
+            $table->dropColumn('gender');
+            $table->dropColumn('employees');
+            $table->dropColumn('sales');
+            $table->dropColumn('industry');
+        });
+    }
+}
