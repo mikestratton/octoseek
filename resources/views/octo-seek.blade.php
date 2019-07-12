75db6b67eccc7a3689ebe6e1fe1197f234e10721
mikestratton <mike@mikestratton.net>
2019-07-12 15:17:32 -0400

    nickel home page

--- resources/views/octo-seek.blade.php
+++ resources/views/octo-seek.blade.php
@@ -4,7 +4,7 @@
     <div class="container text-center">
         <div class="row mt-4">
             <div class="col-md-12">
-                <img class="img-fluid" style="max-height: 200px;" src="images/octoseek-logo.png" alt="octoseek logo">
+                <img class="img-fluid" style="max-height: 200px;" src="images/nickel.png" alt="octoseek logo">
             </div>
 
         </div>
@@ -12,8 +12,16 @@
 
         <div class="row mt-4">
             <div class="col-md-8 offset-md-2">
-                <h1>Business automation software designed to
-                    seek and find pearls of useful data.</h1>
+                <h1>Targeted sales leads for just <strong>5¢</strong> a lead.
+                    <br>Just select your target industry and
+                    location. </h1>
+                <h2>
+                    <a href="csv-sample/apartment-sample.csv">
+                        Click here for a sample download of apartment rental agencies in New York
+                    City, New York</a>
+                </h2>
+                <p>Octoseek.com is under active development.
+                    For inquiries please contact Sunny Tree Software at (330)802-0285</p>
             </div>
         </div>
         <hr>
